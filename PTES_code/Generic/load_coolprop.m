e8eda06766c40c0b117874f2e2471f7e4517f3c9
jmctigue <JoshuaDominic.McTigue@nrel.gov>
2019-09-06 14:37:17 -0600

    Changed load_coolprop so that it adds the correct paths for Coolprop to work under Windows

--- PTES_code/Generic/load_coolprop.m
+++ PTES_code/Generic/load_coolprop.m
@@ -29,5 +29,22 @@ switch computer
     case 'PCWIN64'
         % Addpath to windows CoolProp folder
         %addpath('.\CoolProps\');
-        addpath('..\CoolProp\windows\');
+        %addpath('..\CoolProp\windows\');
+        
+        path_to_lib = '../CoolProp/windows/libs/shared'; %specify path to coolprop shared library
+        path_to_include= '../CoolProp/linux/coolprop/include'; %specify path to coolprop's include folder
+
+        % Loading shared library
+        if ~libisloaded('coolprop') %checking whether library is already loaded
+            addpath(path_to_lib)
+            addpath(path_to_include)
+            libname = 'libCoolProp'; % OSX and linux
+            if ispc
+                libname = 'CoolProp';
+            end
+            loadlibrary(libname,'CoolPropLib.h','includepath',path_to_include,'alias','coolprop'); % loading library with alias coolprop
+            disp('loaded CoolProp shared library.')
+            disp('loaded these functions: ')
+            libfunctions coolprop
+        end
 end
