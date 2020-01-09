cff3ee09871ecc1115f456d120c7d33674c93b95
PFA-the-great <pf298@cam.ac.uk>
2020-01-09 16:04:53 +0000

    Made the 'save_fig' function more versatile, and improved documentation

--- PTES_code/Generic/save_fig.m
+++ PTES_code/Generic/save_fig.m
@@ -1,14 +1,24 @@
-function save_fig( num, file_name, width, height, mode )
-% Set figure size and save in .fig and .eps formats
-% file_name is the file path, e.g. './results/my_figure'
-% Usage example: save_fig(1,'./results/my_figure',0,0,0);
+function save_fig( num, file_name, formats, dimensions )
+% SAVE_FIG
+%   This function saves a figure into specified formats.
+%   "num" is the figure window number
+%   "file_name" is the name of the file to be saved, without format.
+%   "formats" is the list of formats in which to save the file in.
+%   The variable "dimensions" (optional) is used to specify width and
+%   length, in centimeters.
+%
+%   USAGE e.g.:
+%   save_fig(1,'example',{'fig','epsc','svg'},[10,20])
+%   save_fig(1,'example','epsc')
 
-if mode == 0 % default
+if nargin == 3 % default
     width  = 14.8167;
     height = 11.1125;
-elseif mode ==1 % take manually given values
+elseif nargin == 4 % take manually given values
+    width  = dimensions(1);
+    height = dimensions(2);
 else
-    error('mode not implemented')
+    error('Number of inputs not implemented')
 end
 
 figure(num)
@@ -17,9 +27,12 @@ fig.PaperPositionMode = 'manual';
 fig.PaperUnits = 'centimeters';
 fig.PaperPosition = [0 0 width height]; %default size
 
-saveas(fig,file_name,'fig')
-saveas(fig,file_name,'epsc')
-saveas(fig,file_name,'svg')
+if ~iscell(formats)
+    formats = {formats};
+end
+for i=1:length(formats)
+    saveas(fig,file_name,formats{i})
+end
 
 end
 
